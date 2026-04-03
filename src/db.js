41b314c4b4554cec9d92e44d7910b928a9eb88d3
BrunoC30 <bruno01eufonio@gmail.com>
2026-04-03 10:18:53 -0300

    fix: ssl false db

--- src/db.js
+++ src/db.js
@@ -3,7 +3,11 @@ const { Pool } = require('pg');
 
 if (process.env.DATABASE_URL) {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+     ssl: {
+    rejectUnauthorized: false
+  }
+    
   });
 } else {
   pool = new Pool({
@@ -11,7 +15,10 @@ if (process.env.DATABASE_URL) {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port:process.env.DB_PORT 
+    port:process.env.DB_PORT ,
+     ssl: {
+    rejectUnauthorized: false
+  }
   });
 }
 
